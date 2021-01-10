fe83ec41435919ab0d481e0bde1c14b3557e9f2f
botcbr <74988849+botcbr@users.noreply.github.com>
2021-01-09 21:24:29 -0300

    Update TownInfo.vue

--- src/components/TownInfo.vue
+++ src/components/TownInfo.vue
@@ -9,7 +9,7 @@
       }"
     ></li>
     <li v-if="players.length - teams.traveler < 5">
-      Please add more players!
+      Por favor adicione mais jogodares.
     </li>
     <li>
       <span class="meta" v-if="!edition.isOfficial">
