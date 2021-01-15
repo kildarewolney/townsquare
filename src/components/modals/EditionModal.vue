dccacb45309c93ccda19e0af55dc93e60236a562
botcbr <74988849+botcbr@users.noreply.github.com>
2021-01-15 20:30:41 -0300

    Update EditionModal.vue

--- src/components/modals/EditionModal.vue
+++ src/components/modals/EditionModal.vue
@@ -117,7 +117,7 @@ export default {
         ],
         [
           "Madness",
-          "https://gist.githubusercontent.com/botcbr/3397ae5dd4f9747cf4c88823522e0b18/raw/10d41d865b568fd17b032b93e75eb9418b14e109/gistfile1.json"
+          "https://gist.githubusercontent.com/botcbr/3397ae5dd4f9747cf4c88823522e0b18/raw/c645949d46db2526c23c20d706f56fe5880b5a5b/gistfile1.json"
         ]
       ]
     };
