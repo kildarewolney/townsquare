4341add457e1b56808babb52a4ad3364198e1011
botcbr <74988849+botcbr@users.noreply.github.com>
2021-01-16 18:23:25 -0300

    Update ReferenceModal.vue

--- src/components/modals/ReferenceModal.vue
+++ src/components/modals/ReferenceModal.vue
@@ -25,7 +25,7 @@
         </span>
       </li>
     </ul>
-    <div v-for="(teamRoles, team) in rolesGrouped" :key="team" :class="[team]">
+    <div v-for="(teamRoles, team) in rolesGrouped" :key="team" :class="[team.cidadão]">
       <h4>{{ team }}</h4>
       <ul>
         <li v-for="role in teamRoles" :class="[team]" :key="role.id">
